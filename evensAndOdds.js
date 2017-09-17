9cfef4c9719f72abb79867144eaff7318aae6ae0
codingplumber <jason.stickel@live.com>
2017-09-16 21:58:02 -0400

    evensAndOdds

---
+++ evensAndOdds.js
@@ -0,0 +1,7 @@
+// This kata is about converting numbers to their binary or hexadecimal representation. If a number is even, convert it to binary. If a number is odd, convert it to hex.
+
+function evensAndOdds(num){
+	return num % 2 === 0 ? (num >>> 0).toString(2) : num.toString(16);
+}
+
+evensAndOdds(13);
